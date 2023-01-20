5a374e001a2f8222c2ec53148ae2487d1a32747c
5541 <5541@DESKTOP-2R6N1UC>
2023-01-20 17:58:00 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿// Vrund Patel
+// 1/20/23
+// Conditional Statements Asssignment
+
+
+namespace conditional_statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a numeric grade: ");
+
+            try
+            {
+                decimal grade = decimal.Parse(Console.ReadLine());
+                string letter;
+
+                if (grade >= 90)
+                {
+                    letter = "A";
+                    Console.WriteLine("Your expected grade for ISM 4300 is a " + letter);
+                }
+                else if (grade >= 80 && grade < 90)
+                {
+                    letter = "B";
+                    Console.WriteLine("Your expected grade for ISM 4300 is a " + letter);
+                }
+                else if (grade >= 70 && grade < 80)
+                {
+                    letter = "C";
+                    Console.WriteLine("Your expected grade for ISM 4300 is a " + letter);
+                }
+                else if (grade >= 60 && grade < 70)
+                {
+                    letter = "D";  
+                    Console.WriteLine("Your expected grade for ISM 4300 is a " + letter);
+                }
+                else if (grade >= 0 && grade < 60)
+                {
+                    letter = "F";
+                    Console.WriteLine("Your expected grade for ISM 4300 is a " + letter);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Enter a numeric grade: ");
+            }
+
+        }
+    }
+}
